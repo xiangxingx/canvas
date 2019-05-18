1926f3fd99ac61070f4d0a574387981ea6560a9e
xiangxingx <xiangxingx@gmail.com>
2019-05-18 22:05:40 +0800

    触屏设备兼容

--- main.js
+++ main.js
@@ -14,38 +14,69 @@ brush.onclick = function () {
 
 autoSetCanvasSize(canvas)
 
-listenToMouse(canvas)
+listenToUser(canvas)
 
-function listenToMouse(canvas) {
+function listenToUser(canvas) {
   var lastPoint = { x: undefined, y: undefined }
   var using = false // 状态锁，用状态控制事件的发生
 
-  canvas.onmousedown = function (ev) {
-    using = true
-    var x = ev.clientX // x,y相对于视口的位置
-    var y = ev.clientY
-    console.log(eraserEnabled)
-    if (eraserEnabled) {
-      context.clearRect(x, y, 100, 100)
-    } else {
-      lastPoint = { x: x, y: y }
-    }
-  }
-  canvas.onmousemove = function (ev) {
-    var x = ev.clientX
-    var y = ev.clientY
-    if (using) { // 且状态与或状态
+  // 特性检测
+  if (document.body.ontouchstart !== undefined) {
+    canvas.ontouchstart = function (ev) {
+      using = true
+      var x = ev.touches[0].clientX
+      var y = ev.touches[0].clientY
       if (eraserEnabled) {
         context.clearRect(x, y, 100, 100)
       } else {
-        var newPoint = { x: x, y: y }
-        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-        lastPoint = newPoint
+        lastPoint = { x: x, y: y }
       }
     }
-  }
-  canvas.onmouseup = function (ev) {
-    using = false
+    canvas.ontouchmove = function (ev) {
+      var x = ev.touches[0].clientX
+      var y = ev.touches[0].clientY
+      if (using) { // 且状态与或状态
+        if (eraserEnabled) {
+          context.clearRect(x, y, 100, 100)
+        } else {
+          var newPoint = { x: x, y: y }
+          drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+          lastPoint = newPoint
+        }
+      }
+    }
+    canvas.ontouchend = function (ev) {
+      using = false
+    }
+
+  } else {
+    canvas.onmousedown = function (ev) {
+      using = true
+      var x = ev.clientX // x,y相对于视口的位置
+      var y = ev.clientY
+      //console.log(eraserEnabled)
+      if (eraserEnabled) {
+        context.clearRect(x, y, 100, 100)
+      } else {
+        lastPoint = { x: x, y: y }
+      }
+    }
+    canvas.onmousemove = function (ev) {
+      var x = ev.clientX
+      var y = ev.clientY
+      if (using) { // 且状态与或状态
+        if (eraserEnabled) {
+          context.clearRect(x, y, 100, 100)
+        } else {
+          var newPoint = { x: x, y: y }
+          drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+          lastPoint = newPoint
+        }
+      }
+    }
+    canvas.onmouseup = function (ev) {
+      using = false
+    }
   }
 }
 
